330a7e85e3bf031fe3a9512be0fea09073971d02
williamsmith <drwillsmith@gmail.com>
2015-11-03 17:24:53 -0600

    first

---
+++ app/src/main/java/com/example/williamsmith/volleyexample/CustomList.java
@@ -0,0 +1,41 @@
+package com.example.williamsmith.volleyexample;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by williamsmith on 11/3/15.
+ */
+public class CustomList extends ArrayAdapter<String> {
+    private String[] ids;
+    private String[] names;
+    private String[] emails;
+    private Activity context;
+
+    public CustomList(Activity context, String[] ids, String[] names, String[] emails) {
+        super(context, R.layout.list_view_layout, ids);
+        this.context = context;
+        this.ids = ids;
+        this.names = names;
+        this.emails = emails;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.list_view_layout, null, true);
+        TextView textViewId = (TextView) listViewItem.findViewById(R.id.textViewId);
+        TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewName);
+        TextView textViewEmail = (TextView) listViewItem.findViewById(R.id.textViewEmail);
+
+        textViewId.setText(ids[position]);
+        textViewName.setText(names[position]);
+        textViewEmail.setText(emails[position]);
+
+        return listViewItem;
+    }
+}
